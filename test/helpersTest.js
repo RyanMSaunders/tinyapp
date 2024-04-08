82543a974529c692586927cacfac5168858fbc34
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-08 17:25:02 +0000

    Add test to getUserByEmail to check if user is found, if user is not found

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,7 +1,7 @@
 
 const { assert } = require('chai');
 
-const getUserByEmail = require('../helpers.js');
+const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -20,15 +20,13 @@ describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers)
     const expectedUserID = "userRandomID";
-    // Write your assert statement here
-    assert(user.id == expectedUserID, 'User found');
+    assert(user.id === expectedUserID, 'User found');
 
   });
   it('should return undefined with email that is not valid', function() {
     const user = getUserByEmail("userxyz@example.com", testUsers)
-    // const expectedUserID = "userRandomID";
-    // Write your assert statement here
     assert(!user, 'User not found');
 
   });
 });
+
